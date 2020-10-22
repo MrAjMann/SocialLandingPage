65b89fccc075e12caba8bc75fe814fad6ddb794c
MrAjMann <anthonyjmann87@gmail.com>
2020-10-22 11:56:39 +1000

    Started adding input field for twitter

--- tweet.js
+++ tweet.js
@@ -9,6 +9,14 @@ var T = new Twit({
   access_token_secret: process.env.access_token_secret
 });
 
+let input = document.createElement('input');
+input.setAttribute('type', 'text');
+
+const innerTwitterContainer = document.getElementById('innerTwitterContainer');
+const twitterContainer = document.getElementsByClassName('twitterContainer');
+innerTwitterContainer.appendChild('input');
+twitterContainer.appendChild('innerTwitterContainer');
+
 // T.post(
 //   'statuses/update',
 //   { status: 'hello world! This is sent using the twitter API' },
@@ -22,3 +30,4 @@ var stream = T.stream('statuses/filter', { track: 'Javascript' });
 stream.on('tweet', function(tweet) {
   console.log(tweet);
 });
+
